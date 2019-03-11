e2ddf9d312c8415713bd361e5b6e49671beeb3cc
Jaka Munda <jaka.munda@siol.net>
2019-03-11 09:30:01 +0100

    predavanja

--- Predavanja/UvodObjektnoProgramiranje/src/myholders/MyHolder.java
+++ Predavanja/UvodObjektnoProgramiranje/src/myholders/MyHolder.java
@@ -2,12 +2,12 @@ package myholders;
 
 import java.util.ArrayList;
 
-public class MyHolder<E> {
+public abstract class MyHolder<E> {
 
 	// tu samo definiramo seznam oz. holder
 	// in naèin kako se notr daje elemente, ker je za to skoz ista metoda
 	
-	ArrayList<E> holder; // ne damo ne private in ne public ker hoèmo da je vidn v podrazredih, vendar ne pa kr nekje drugje
+	protected ArrayList<E> holder; // ne damo ne private in ne public ker hoèmo da je vidn v podrazredih, vendar ne pa kr nekje drugje
 	
 	public MyHolder() {
 		holder = new ArrayList<E> (); // naredimo prazen seznam
@@ -16,5 +16,7 @@ public class MyHolder<E> {
 	public void put (E val) {
 		holder.add(0, val); // morš dat 0 da postavi nov element na zaèetek drugaèe ga postavi na konec
 	}
+	
+	public abstract E take (); // z tem sem v superclass dal metodo take vendar
 
 }
